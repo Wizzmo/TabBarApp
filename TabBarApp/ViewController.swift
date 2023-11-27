c9e891a6cbb3caed4bff1ec1af3cc361c20fd05d
Maxim Makarov <makarovmaxqa@gmail.com>
2023-11-27 22:00:59 +0100

    Initial Commit

---
+++ TabBarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarApp
+//
+//  Created by Maxim Makarov on 27.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
